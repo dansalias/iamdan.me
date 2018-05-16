9af619366ccba20d76892ebb72611a14b5ecc317
d4n13ly <kiwidan92@gmail.com>
2018-05-16 23:26:38 +1200

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <div id="profile-image">
+      <img src="images/me.jpg">
+    </div>
+    <h1>Hello! I am Dan.</h1>
+    <p>I am a software developer currently based in Auckland, New&nbsp;Zealand.</p>
+    <p>I specialise in full-stack web application, development operations and cloud architecture.</p>
+  </header>
+</template>
