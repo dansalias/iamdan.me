9704be4c01d1ac4201a591ded0985738089b6060
d4n13ly <kiwidan92@gmail.com>
2018-05-16 23:56:21 +1200

    Update header message

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,6 @@
     </div>
     <h1>Hello! I am Dan.</h1>
     <p>I am a software developer currently based in Auckland, New&nbsp;Zealand.</p>
-    <p>I specialise in full-stack web application, development operations and cloud architecture.</p>
+    <p>I specialise in Full-Stack Web Application Development, DevOps and Cloud Architecture.</p>
   </header>
 </template>
