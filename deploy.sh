25e1b0d177f732ae4ac2b9342e03dff2b4be671b
dansalias <danyoungnz@gmail.com>
2020-03-01 17:44:54 +0100

    Update origin name in deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ cp -R ../dist/* .
 echo "iamdan.me" >> CNAME
 git add .
 git commit -m "Deploy to gh-pages at $(git log '--format=format:%H' master -1)"
-git push github gh-pages
+git push origin gh-pages
